4d465d0b1106a4164988bf20988ee75a8852fa06
sarahck1 <sarahck1@outlook.com>
2025-05-01 14:55:12 -0500

    added health script and text, no enemies yet

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class playerHealth : MonoBehaviour
+{
+    public int health = 3;
+    public Text healthText;
+
+    void Start()
+    {
+        UpdateHealthDisplay();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateHealthDisplay();
+
+        if (health <= 0)
+        {
+            Debug.Log("Game Over");
+            
+        }
+    }
+
+    void UpdateHealthDisplay()
+    {
+        healthText.text = "Health: " + health;
+    }
+}
